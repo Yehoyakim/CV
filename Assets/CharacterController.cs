0a3f162a66a3af02ef994138ff7379120dbbd869
unknown <joaquin_gebauer@hotmail.com>
2018-05-17 11:19:46 -0400

    initial commit

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+	private Rigidbody2D rb;
+	private SpriteRenderer sRenderer;
+	private Animator animator;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		sRenderer = GetComponent<SpriteRenderer>();
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Input.GetKey(KeyCode.D)){
+			rb.AddForce(Vector2.right * speed);
+			sRenderer.flipX = false;
+			animator.SetBool("IsWalking",true);
+		}else if(Input.GetKey(KeyCode.A)){
+			animator.SetBool("IsWalking",true);
+			sRenderer.flipX = true;
+			rb.AddForce(-Vector2.right * speed);
+		}else{
+			animator.SetBool("IsWalking",false);
+		}
+	}
+}

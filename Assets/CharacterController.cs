3094cd7895f780b965e9faf1d7c0cc7a92ab0ecd
unknown <joaquin_gebauer@hotmail.com>
2018-05-18 00:09:13 -0400

    agregado salto y limpieza de codigo

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -2,31 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour {
-	private Rigidbody2D rb;
-	private SpriteRenderer sRenderer;
-	private Animator animator;
-	public float speed;
+public class CharacterController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private SpriteRenderer sRenderer;
+    private Animator animator;
+	public float maxSpeed;
+    public float speed;
+    public float jumpForce;
 
-	// Use this for initialization
-	void Start () {
-		rb = GetComponent<Rigidbody2D>();
-		sRenderer = GetComponent<SpriteRenderer>();
-		animator = GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		if(Input.GetKey(KeyCode.D)){
-			rb.AddForce(Vector2.right * speed);
-			sRenderer.flipX = false;
-			animator.SetBool("IsWalking",true);
-		}else if(Input.GetKey(KeyCode.A)){
-			animator.SetBool("IsWalking",true);
-			sRenderer.flipX = true;
-			rb.AddForce(-Vector2.right * speed);
-		}else{
-			animator.SetBool("IsWalking",false);
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            move(-Vector2.left);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+           move(Vector2.left);
+        }
+        else
+        {
+            animator.SetBool("IsWalking", false);
+        }
+
+
+        if (Input.GetKey(KeyCode.W) && animator.GetBool("IsGrounded"))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            animator.Play("salto");
+        }
+
+    }
+
+    private void move(Vector2 direction)
+    {
+		if(rb.velocity.magnitude < maxSpeed && animator.GetBool("IsGrounded")){
+			
+		
+        animator.SetBool("IsWalking", true);
+        rb.AddForce(direction * speed);
+        if (direction.x > 0)
+        {
+            sRenderer.flipX = false;
+        }
+        else
+        {
+            sRenderer.flipX = true;
+        }
 		}
-	}
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        animator.SetBool("IsGrounded", true);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        animator.SetBool("IsGrounded", false);
+    }
 }

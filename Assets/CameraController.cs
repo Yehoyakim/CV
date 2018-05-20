6495e81ff9cbc3b20045ce8fcbc407f59634b907
joaquin gebauer <joaquin_gebauer@hotmail.com>
2018-05-20 01:07:12 -0400

    agregado camera follow y piso

---
+++ Assets/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private GameObject player;
+
+    [Range(0,1)]
+    public float cameraSpeed;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+        Vector3 target = player.transform.position - new Vector3(0,0,10) ;
+        transform.position = Vector3.Lerp(transform.position, target, cameraSpeed);
+	}
+}
--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -69,6 +69,7 @@ public class CharacterController : MonoBehaviour
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        animator.SetBool("IsGrounded", false);
+            animator.SetBool("IsGrounded", false);
+       
     }
 }
